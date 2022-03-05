9d73a8694b9dee30f31d0bfd3fc2685188bd254e
Sophie Snow <sophie.snoww@pm.me>
2022-03-05 15:32:57 -0800

    Started creaton of actual homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -5,18 +5,18 @@ import "./Homepage.css";
 function Homepage(props) {
     return (
         <div className="homepage-container">
-            <Square size="400px" speed="7s" opacity="1" />
-            <Square size="425px" speed="12s" opacity=".9" />
-            <Square size="450px" speed="16s" opacity=".5" />
-
-            <Circle size="300px" speed="5s" opacity=".2" />
-            <Circle size="400px" speed="11s" opacity=".5" />
-            <Circle size="500px" speed="14s" opacity="1" />
-
-            <div className="homepage-title">
-                Wy+vern
-                <br />
-                ~
+            <div className="homepage-content-container">
+                <div className="homepage-titlebar">
+                    <img className="homepage-titlebar-logo" src="/logo.svg" alt="Wyvern Logo" />
+                    <div className="homepage-titlebar-items">
+                        <div className="homepage-titlebar-item">Home</div>
+                        <div className="homepage-titlebar-item">About</div>
+                        <div className="homepage-titlebar-item">Log In</div>
+                    </div>
+                </div>
+                <div className="homepage-content">
+                    yeah    
+                </div>
             </div>
         </div>
     );
