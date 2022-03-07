9d586fbe4319ba56652d0f2b51093d01b70772c1
Sophie Snow <sophie.snoww@pm.me>
2022-03-06 20:09:25 -0700

    Added 404 page

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Homepage from './components/homepage/Homepage';
+import Error404 from "./components/error404/Error404";
 
 import './AppContainer.css';
 
@@ -9,7 +10,7 @@ function AppContainer() {
         <div className="app-container">
             <Routes>
                 <Route path="/" element={<Homepage />} />
-                <Route path="/*" element={<Homepage />} />
+                <Route path="/*" element={<Error404 />} />
             </Routes>
         </div>
     );
