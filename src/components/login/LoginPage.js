3ac8575a02ade2d8d3e6a480331f42d388b7d410
Sylas <38060892+glacierheavner@users.noreply.github.com>
2022-03-08 16:13:22 -0700

    Started work on real login logic

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -9,19 +9,51 @@ class LoginPage extends React.Component {
         this.state = {
             username: "",
             password: "",
-            createAccount: false
+            passwordConfirm: "",
+            createAccount: false,
+            alertText: ":)",
+            alertTextEnabled: false
         }
     }
 
     handleChange(e) {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: (e.target.name === "createAccount") ? (e.target.checked) : (e.target.value)
         });
     }
 
     handleFormSubmit(e) {
         e.preventDefault();
-        console.log(this.state);
+
+        if (this.state.createAccount) {
+            if ([this.state.username, this.state.password, this.state.passwordConfirm].includes("")) {
+                return this.setState({
+                    alertText: "You must fill in all boxes!",
+                    alertTextEnabled: true
+                });
+            }
+
+            if (this.state.password !== this.state.passwordConfirm) {
+                return this.setState({
+                    alertText: "Your passwords don't match!",
+                    alertTextEnabled: true
+                });
+            }
+        }
+        else {
+            if ([this.state.username, this.state.password].includes("")) {
+                return this.setState({
+                    alertText: "You must fill in all boxes!",
+                    alertTextEnabled: true
+                });
+            }
+        }
+
+        console.log(`\
+        USERNAME: ${this.state.username}
+        PASSWORD: ${this.state.password}
+        CREATE ACCOUNT? : ${this.state.createAccount}\
+        `.replace(/[ ]{2,}/gm, ""));
     }
 
     render() {
@@ -29,9 +61,23 @@ class LoginPage extends React.Component {
             <div className="login-page">
                 <ParticlesPreset />
                 <form className="login-form" onSubmit={(e) => this.handleFormSubmit(e)}>
-                    <input type="text" autoComplete="off" name="username" className="login-input login-username-input" value={this.state.username} onChange={(e) => this.handleChange(e)} />
-                    <input type="password" autoComplete="off" name="password" className="login-input login-password-input" value={this.state.password} onChange={(e) => this.handleChange(e)} />
-                    <button type="submit" className="login-input login-button">Log In</button>
+                    <div className="alert-text" style={{
+                        opacity: this.state.alertTextEnabled ? 1 : 0
+                    }}>{this.state.alertText}</div>
+
+                    <input type="text" placeholder="Username" autoComplete="off" name="username" className="login-input login-username-input" value={this.state.username} onChange={(e) => this.handleChange(e)} />
+
+                    <input type="password" placeholder="Password" autoComplete="off" name="password" className="login-input login-password-input" value={this.state.password} onChange={(e) => this.handleChange(e)} />
+                    <input type="password" placeholder="Confirm Password" autoComplete="off" name="passwordConfirm" className="login-input login-password-input" value={this.state.passwordConfirm} onChange={(e) => this.handleChange(e)} style={{
+                        opacity: this.state.createAccount ? 1 : 0
+                    }} />
+
+                    <button type="submit" className="login-input login-button">{(this.state.createAccount) ? "Create Account" : "Log In"}</button>
+
+                    <div className="create-account">
+                        Create account?
+                        <input type="checkbox" name="createAccount" className="create-account-checkbox" value={this.state.createAccount} onChange={(e) => this.handleChange(e)} />
+                    </div>
                 </form>
             </div>
         );
